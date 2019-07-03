6cc75c5f69698f1643641a1eb8ca146b70b3e279
Sam <saudino480@gmail.com>
2019-07-03 14:16:36 -0400

    beginning process of clicking through filter options

--- grailed_scrapper.py
+++ grailed_scrapper.py
@@ -4,22 +4,37 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
+from helper import *
 import csv
 import re
 import time
 import random
 
-driver = webdriver.Chrome(r"C:/chromedriver/chromedriver.exe")
+#driver = webdriver.Chrome(r"C:/chromedriver/chromedriver.exe")
+driver = webdriver.Chrome()
 
 driver.get("https://www.grailed.com/categories/short-sleeve-t-shirts")
 
 driver.execute_script("window.scrollTo(0,1000)")
 
-total_items_raw = driver.find_element_by_xpath('//h3[@class="-summary"]').text
-temp = re.search("\d+", total_items_raw)
-total_items = int(temp.group())
-total_pages = total_items // 98 + 1
-print(total_pages)
+time.sleep(2)
+
+ActionChains(driver).move_to_element(driver.find_element_by_xpath('//div[@class="designers-wrapper _collapsed"]')).perform()
+
+designer_menu = driver.find_element_by_xpath('//div[@class="designers-wrapper _collapsed"]')
+designer_menu_button = test.find_element_by_xpath('.//div[@class="-collapsible-target"]')
+designer_menu_button.click()
+
+desisgner_cat_button = driver.find_element_by_xpath('//button[@title="View all designers"]')
+desisgner_cat_button.click()
+
+temp = designer_cat.find_element_by_xpath('.//div[@class="content"]')
+designers_list = temp.find_elements_by_xpath('.//div[@class="active-indicator"]')
+
+
+total_scrolls = how_many_scrolls(driver)
+print(total_scrolls)
+
 
 csv_file = open('hypebeast.csv', 'w', encoding='utf-8', newline='')
 writer = csv.writer(csv_file)
@@ -38,68 +53,13 @@ while True:
 		#actual timer for implementation
 		time.sleep(random.randint(4, 8))
 
-
 		listings = driver.find_elements_by_xpath('//div[@class="feed-item"]')
 		print("Listings has: ", len(listings))
 		ActionChains(driver).move_to_element(driver.find_element_by_xpath('.//div[@class="feed-item empty-item"]')).perform()
 
-		if (last_scrapped in listings):
-			idx = listings.index(last_scrapped) + 1
-			#listings = listings[idx:]
-			print("I trimmed my list of ", str(idx), " entries!")
-		else:
-			print("I did not trim my list :(")
-		last_scrapped = listings[-1]
+		listings, last_scrapped = listing_cleaner(listings, last_scrapped)
 
-		#print(listings[30:32])
-		for listing in listings:
-			listing_dict = {}
-			designer = listing.find_element_by_xpath('.//h3[@class="listing-designer truncate"]').text
-			description = listing.find_element_by_xpath('.//h3[@class="sub-title listing-title"]/div').text
-			size = listing.find_element_by_xpath('.//h3[@class="listing-size sub-title"]').text
-
-			#price is listed two ways, and the first will fail if it has been
-			#listed before.
-			try:
-				price = listing.find_element_by_xpath('.//h3[@class="sub-title original-price"]/span').text
-			except:
-				original_price = listing.find_element_by_xpath('.//h3[@class="sub-title original-price strike-through"]/span').text
-				price = listing.find_element_by_xpath('.//h3[@class="sub-title new-price"]/span').text
-
-			#dates are sorted two ways, we want to make sure that we can get
-			#the repost data as well.
-			try:
-				date = listing.find_element_by_xpath('.//span[@class="date-ago"]').text
-				old_date = listing.find_element_by_xpath('.//span[@class="strike-through"]').text
-			except:
-				date = listing.find_element_by_xpath('.//span[@class="date-ago"]').text
-
-
-			url = listing.find_element_by_xpath('.//a').get_attribute('href')
-
-			listing_dict['designer'] = designer
-			listing_dict['description'] = description
-			listing_dict['size'] = size
-			listing_dict['url'] = url
-
-			#fill the dictionary options based on what happened above
-			try:
-				listing_dict['original_price'] = original_price
-				listing_dict['price'] = price
-			except:
-				listing_dict['original_price'] = ""
-				listing_dict['price'] = price
-
-
-			try:
-				listing_dict['date'] = date
-				listing_dict['old_date'] = old_date
-			except:
-				listing_dict['date'] = date
-				listing_dict['old_date'] = ""
-
-			writer.writerow(listing_dict.values())
-
+		listing_processing(listings, writer)
 
 		# move to the next page after we get all the information we need. (LEGACY)
 		#new_page = "https://www.grailed.com/categories/short-sleeve-t-shirts" + "?page=" + str(index)
--- helper.py
+++ helper.py
@@ -6,39 +6,77 @@ from selenium.webdriver.support import expected_conditions as EC
 import csv
 import re
 
-def page_scroll():
+def listing_cleaner(listings, last_scrapped):
+    if (last_scrapped in listings):
+        idx = listings.index(last_scrapped) + 1
+        listings = listings[idx:]
+        print("I trimmed my list of ", str(idx), " entries!")
+    else:
+        print("I did not trim my list :(")
+    last_scrapped = listings[-1]
+    return listings, last_scrapped
 
-  while True:
 
-    # Get scroll height
-    ### This is the difference. Moving this *inside* the loop
-    ### means that it checks if scrollTo is still scrolling
-    last_height = driver.execute_script("return document.body.scrollHeight")
+def how_many_scrolls(webdriver):
+    total_items_raw = webdriver.find_element_by_xpath('//h3[@class="-summary"]').text
+    temp = re.search("\d+", total_items_raw)
+    total_items = int(temp.group())
+    print(total_items)
 
-    # Scroll down to bottom
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    listings = webdriver.find_elements_by_xpath('//div[@class="feed-item"]')
+    items_on_page = len(listings)
+    print(items_on_page)
 
-    # Wait to load page
-    time.sleep(SCROLL_PAUSE_TIME)
+    return((total_items // items_on_page) + 1)
 
-    # Calculate new scroll height and compare with last scroll height
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
 
-        # try again (can be removed)
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-        # Wait to load page
-        time.sleep(SCROLL_PAUSE_TIME)
 
-        # Calculate new scroll height and compare with last scroll height
-        new_height = driver.execute_script("return document.body.scrollHeight")
+def listing_processing(listings, writer):
+    for listing in listings:
+        listing_dict = {}
+        designer = listing.find_element_by_xpath('.//h3[@class="listing-designer truncate"]').text
+        description = listing.find_element_by_xpath('.//h3[@class="sub-title listing-title"]/div').text
+        size = listing.find_element_by_xpath('.//h3[@class="listing-size sub-title"]').text
 
-        # check if the page height has remained the same
-        if new_height == last_height:
-            # if so, you are done
-            break
-        # if not, move on to the next loop
-        else:
-            last_height = new_height
-            continue
+        #price is listed two ways, and the first will fail if it has been
+        #listed before.
+        try:
+            price = listing.find_element_by_xpath('.//h3[@class="sub-title original-price"]/span').text
+        except:
+            original_price = listing.find_element_by_xpath('.//h3[@class="sub-title original-price strike-through"]/span').text
+            price = listing.find_element_by_xpath('.//h3[@class="sub-title new-price"]/span').text
+
+        #dates are sorted two ways, we want to make sure that we can get
+        #the repost data as well.
+        try:
+            date = listing.find_element_by_xpath('.//span[@class="date-ago"]').text
+            old_date = listing.find_element_by_xpath('.//span[@class="strike-through"]').text
+        except:
+            date = listing.find_element_by_xpath('.//span[@class="date-ago"]').text
+
+
+        url = listing.find_element_by_xpath('.//a').get_attribute('href')
+
+        listing_dict['designer'] = designer
+        listing_dict['description'] = description
+        listing_dict['size'] = size
+        listing_dict['url'] = url
+
+        #fill the dictionary options based on what happened above
+        try:
+            listing_dict['original_price'] = original_price
+            listing_dict['price'] = price
+        except:
+            listing_dict['original_price'] = ""
+            listing_dict['price'] = price
+
+
+        try:
+            listing_dict['date'] = date
+            listing_dict['old_date'] = old_date
+        except:
+            listing_dict['date'] = date
+            listing_dict['old_date'] = ""
+
+        writer.writerow(listing_dict.values())
